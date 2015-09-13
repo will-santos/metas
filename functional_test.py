ce8066f0c478daa987336597fa50be1333a2c2a5
w.santos <w.santos.python@gmail.com>
2015-09-13 11:26:10 -0300

    organizando estrutura do teste com unittest

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:5000')
 
-assert 'Metas' in browser.title
+class TestNovoUsuario(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_adicionar_um_novo_item_de_compra(self):
+        self.browser.get('http://localhost:5000')
+
+        # Na aba de titulos ele vê a palavra Metas
+        self.assertIn('Metas', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
