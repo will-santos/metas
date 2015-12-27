52b59a3a97f350d768c774d2651cfc6ec0d71780
will <w.santos.python@gmail.com>
2015-12-26 23:55:17 -0200

    update test

--- metas.py
+++ metas.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return '<html><head><title>Metas</title></head></html>'
+    return render_template('main.html')
+
+@app.route('/meta/')
+@app.route('/meta/<meta>')
+def metas(meta=None):
+	if meta is None:
+		return 'Todas as metas <list>'
+	return 'Meta {}'.format(meta)
 
 if __name__ == '__main__':
     app.run(debug=True)
